4a621e8842609900f89c0f3d358e909fb42cb4fc
daleeg <xph_wangly10@163.com>
2021-05-14 16:56:21 +0800

    Update single.go

--- design/singleton/single.go
+++ design/singleton/single.go
@@ -11,8 +11,8 @@ var single *singleton
 var once sync.once
 
 func GetInstance() *singleton {
-	once.Do(func() {
-		instance = &singleton{}
-	})
-	returninstance
+    once.Do(func() {
+        instance = &singleton{}
+    })
+    return instance
 }
